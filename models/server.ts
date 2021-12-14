f7d60dc4bf32432797466cd22b3a80519674bfdc
Felipe Navas <felipenavas.itec@gmail.com>
2021-12-14 12:42:08 -0300

    Adding controllers and routes

--- models/server.ts
+++ models/server.ts
@@ -1,18 +1,29 @@
 import express, { Application } from 'express';
+import userRoutes from '../routes/usuarios';
 
 class Server {
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    };
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+
+        // Defino mis rutas
+        this.routes();
+    };
+
+    routes(){
+        this.app.use(this.apiPaths.usuarios, userRoutes);
     };
 
     listen() {
         this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en el puerto' + this.port);
+            console.log('Servidor corriendo en el puerto ' + this.port);
         });
     };
 };
