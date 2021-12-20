3e857905e0fb1cc808f60fc27e91c9caba65d7df
Felipe Navas <felipenavas.itec@gmail.com>
2021-12-20 11:17:23 -0300

    Adding post put and delete

--- models/usuario.ts
+++ models/usuario.ts
@@ -1,5 +1,5 @@
-import { DataTypes } from "sequelize";
-import db from "../db/connection";
+import { DataTypes } from 'sequelize';
+import db from '../db/connection';
 
 const Usuario = db.define('Usuario', {
     nombre: {
